1c7dd341699386c91c501cbebbf0375e01b5775f
vikassj <vikassangwan652@gmail.com>
2024-01-14 15:22:39 +0530

    first commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>VikasAdmin</span>
+      <span>© Vikas Dev Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
